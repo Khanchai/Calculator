1fd77d481f5ee39efdae7aa3017734227b765875
khanchai <khanchai.c@ssd.co.th>
2013-09-05 16:44:31 +0700

    create Calcultor Project

---
+++ Calculator/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 0;
+            var calculator = new Calculator();
+
+            Console.WriteLine("Input Operator(+, -, *, /)");
+            Console.Write("> ");
+            var inputOperator = Console.ReadLine();
+
+            while (true)
+            {
+                Console.WriteLine("Input Operand(If you want to exist, input exit)");
+                Console.Write("> ");
+                var inputNumber = Console.ReadLine();
+
+                switch (inputNumber.Contains(","))
+                {
+                        
+                }
+
+                if (inputNumber != "exit")
+                {
+                    calculator.AddInput(Convert.ToInt32(inputNumber));
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Calculator
+    {
+        public enum Operator { Plus, Minus, Multiply, Divison }
+        public int Number { get; set; }
+
+        public void AddInput(int number)
+        {
+            number = number;
+
+        }
+
+        public int[] AddInputs(int[] numbers)
+        {
+            
+            return numbers;
+        }
+
+        public void Calculate()
+        {
+            
+        }
+    }
+}
