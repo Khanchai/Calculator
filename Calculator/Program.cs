c47bc8c81a354a190f8edaf13f487eb18369bf43
khanchai <khanchai.c@ssd.co.th>
2013-09-06 11:23:10 +0700

    calculator success

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -2,30 +2,46 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Net.Mime;
+using System.Reflection;
 using System.Reflection.Emit;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Calculator
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            var calculator = new Calculator();
-            var plus = Calculator.Operator.Plus;
-            var minus = Calculator.Operator.Minus;
-            var multiply = Calculator.Operator.Multiply;
-            var divison = Calculator.Operator.Divison;
+            Calculator calculator = null;
+            var reg = new Regex(",*[0-9]*");
 
-            var listNumber = new List<int>();
-
+            inputAgain:
             Console.WriteLine("Input Operator(+, -, *, /)");
             Console.Write("> ");
+            
             var inputOperator = Console.ReadLine();
-
+            switch (inputOperator)
+            {
+                case "+":
+                    calculator = new Calculator(OperatorType.Plus);
+                    break;
+                case "-":
+                    calculator = new Calculator(OperatorType.Minus);
+                    break;
+                case "*":
+                    calculator = new Calculator(OperatorType.Multiply);
+                    break;
+                case "/":
+                    calculator = new Calculator(OperatorType.Divison);
+                    break;
+                default :
+                    goto inputAgain;
+                    break;
+            }
 
             while (true)
             {
@@ -33,50 +49,108 @@ namespace Calculator
                 Console.Write("> ");
                 var inputNumber = Console.ReadLine();
 
-               if(inputNumber != "exit")
+                if (inputNumber != null && (inputNumber != "exit" && !inputNumber.Contains(",")))
                 {
-                    if (inputNumber != null && inputNumber.Contains(","))
+                    calculator.AddInput(Convert.ToInt32(inputNumber));
+                }
+                else if (inputNumber != null && inputNumber.Contains(","))
+                {
+                    var sp = inputNumber.Split(new char[] {','});
+                    var numArr = new int[sp.Length];
+                    for (var i = 0; i < sp.Length; i++)
                     {
-//                        calculator.AddInputs();
+                        numArr[i] = Convert.ToInt32(sp[i]);
                     }
-                    else  
-                    {
-                        calculator.AddInput(Convert.ToInt32(inputNumber));
-                    }
+                    calculator.AddInputs(numArr);
+                }
+                else
+                {
+                    break;
                 }
 
-//                if (inputNumber != "exit")
-//                {
-//                    calculator.AddInput(Convert.ToInt32(inputNumber));
-//                }
-//                else
-//                {
-//                    break;
-//                }
             }
-         
+            Console.WriteLine("> Result : "+"{0} = {1}",calculator,calculator.Calculate());
+
             Console.ReadKey();
         }
     }
 
+    public enum OperatorType { Plus, Minus, Multiply, Divison }
+
     class Calculator
     {
-        public enum Operator { Plus, Minus, Multiply, Divison }
+        private List<int> operands = new List<int>();
+
+        public OperatorType Operator { get; private set; }
+
+        public Calculator(OperatorType op)
+        {
+            Operator = op;
+        }
 
         public void AddInput(int number)
         {
-
+            operands.Add(number);
         }
 
         public void AddInputs(int[] numbers)
         {
-
+            operands.AddRange(numbers);
         }
 
-        public void Calculate()
+        public int Calculate()
         {
+            var result = operands.First();
+            switch (Operator)
+            {
+                case OperatorType.Plus:
+                    foreach (var i in operands.Skip(1))
+                    {
+                        result = result + i;
+                    }
+                    break;
+                case OperatorType.Minus:
+                    foreach (var i in operands.Skip(1))
+                        result = result - i;
+                    break;
+                case OperatorType.Multiply:
+                    foreach (var i in operands.Skip(1))
+                    {
+                        result = result * i;
+                    }
+                    break;
+                case OperatorType.Divison:
+                    foreach (var i in operands.Skip(1))
+                    {
+                        result = result / i;
+                    }
+                    break;
+            }
+            return result;
+        }
+
+        public override string ToString()
+        {
+            var separater = "";
+
+            switch (Operator)
+            {
+                case OperatorType.Plus:
+                    separater = "+";
+                    break;
+                case OperatorType.Minus:
+                    separater = "-";
+                    break;
+                case OperatorType.Multiply:
+                    separater = "*";
+                    break;
+                case OperatorType.Divison:
+                    separater = "/";
+                    break;
+            }
+
+            return String.Join(separater,operands);
 
         }
-        
     }
 }
